ce625d61fb763a3876b282f216b3ae528abdad41
Carlo Zamagni <carlozamagni@gmail.com>
2013-05-27 13:21:00 +0200

    project import

---
+++ SwissArmyKnife/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissArmyKnife.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime GetNextDateForDay(this DateTime startDate, DayOfWeek desiredDay)
+        {
+            DateTime nextDate = startDate;
+            while (nextDate.DayOfWeek != desiredDay)
+                nextDate = nextDate.AddDays(1D);
+
+            return nextDate;
+        }
+
+        public static DateTime GetNextSunday(this DateTime fromDate)
+        {
+            DateTime date = new DateTime(year: fromDate.Year, month: fromDate.Month, day: fromDate.Day, hour: 0, minute: 1, second: 0);
+
+            return date.AddDays(7 - (date.DayOfWeek == DayOfWeek.Sunday ? 7 : (int)date.DayOfWeek));
+        }
+
+        public static DateTime GetLastMonday(this DateTime fromDate)
+        {
+            DateTime date = new DateTime(year: fromDate.Year, month: fromDate.Month, day: fromDate.Day, hour: 0, minute: 1, second: 0);
+
+            return date.Subtract(new TimeSpan((int)date.DayOfWeek - 1, 0, 0, 0, 0));
+        }
+    }
+}
---
+++ SwissArmyKnife/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SwissArmyKnife.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        /*
+         * Thanks to Matthew Vines: http://stackoverflow.com/questions/8582344/does-c-sharp-have-isnullorempty-for-list-ienumerable
+         */
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null)
+            {
+                return true;
+            }
+
+            /* If this is a list, use the Count property for efficiency. 
+             * The Count property is O(1) while IEnumerable.Count() is O(N). */
+            var collection = enumerable as ICollection<T>;
+            if (collection != null)
+            {
+                return collection.Count < 1;
+            }
+            return !enumerable.Any();
+        }
+
+        public static void Shuffle<T>(this IList<T> enumerable)
+        {
+            Random rng = new Random();
+            int n = enumerable.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = enumerable[k];
+                enumerable[k] = enumerable[n];
+                enumerable[n] = value;
+            }
+        }
+    }
+}
